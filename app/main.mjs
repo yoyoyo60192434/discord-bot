e1bf73042a591290cc2fe37ff3f8c06e7e704759
yoyoyo60192434 <163037167+yoyoyo60192434@users.noreply.github.com>
2026-01-22 11:09:15 +0900

    Create main.mjs

---
+++ app/main.mjs
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
